721246191459cd188da440283a9afc3be47a18d0
NijeboerFrank <frank.nijeboer@gmail.com>
2023-11-29 16:30:26 +0100

    Implement RNG

---
+++ ta/src/random.rs
@@ -0,0 +1,27 @@
+// OP-TEE TA imports
+use optee_utee::crypto_op::Random;
+
+use rand_core::CryptoRng;
+use rand_core::{RngCore, Error};
+
+pub struct PatatRng;
+
+impl RngCore for PatatRng {
+    fn next_u32(&mut self) -> u32 {
+        unimplemented!()
+    }
+
+    fn next_u64(&mut self) -> u64 {
+        unimplemented!()
+    }
+
+    fn fill_bytes(&mut self, dest: &mut [u8]) {
+        Random::generate(dest);
+    }
+
+    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), Error> {
+        unimplemented!()
+    }
+}
+
+impl CryptoRng for PatatRng {}
